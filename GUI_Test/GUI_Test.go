5475d5779be330e55a62192addfbc27e9e4abea8
Carl <carlangelou06@gmail.com>
2024-02-12 13:57:40 -0500

    Added Button

--- GUI_Test/GUI_Test.go
+++ GUI_Test/GUI_Test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"image/color"
+	//"image/color"
 	"log"
 	"os"
 
@@ -9,25 +9,33 @@ import (
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/text"
+	//"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
 
 func main() {
 	go func() {
-		w := app.NewWindow()
+		w := app.NewWindow(
+			app.Title("Current Person"),
+			app.Size(unit.Dp(400), unit.Dp(200)),
+		)
+
 		err := run(w)
 		if err != nil {
 			log.Fatal(err)
 		}
 		os.Exit(0)
 	}()
+
 	app.Main()
 }
 
 func run(w *app.Window) error {
 	th := material.NewTheme()
 	var ops op.Ops
+	var startButton widget.Clickable
 	for {
 		switch e := w.NextEvent().(type) {
 		case system.DestroyEvent:
@@ -36,18 +44,26 @@ func run(w *app.Window) error {
 			// This graphics context is used for managing the rendering state.
 			gtx := layout.NewContext(&ops, e)
 
-			// Define an large label with an appropriate text:
-			title := material.H1(th, "Hello, Gio")
-
-			// Change the color of the label.
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-
-			// Change the position of the label.
-			title.Alignment = text.Middle
-
-			// Draw the label to the graphics context.
-			title.Layout(gtx)
+			layout.Flex{
+				// Vertical alignment, from top to bottom
+				Axis: layout.Vertical,
+				// Empty space is left at the start, i.e. at the top
+				Spacing: layout.SpaceStart,
+			}.Layout(gtx,
+				// We insert two rigid elements:
+				// First a button ...
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						btn := material.Button(th, &startButton, "Start")
+						return btn.Layout(gtx)
+					},
+				),
+				// ... then an empty spacer
+				layout.Rigid(
+					// The height of the spacer is 25 Device independent pixels
+					layout.Spacer{Height: unit.Dp(25)}.Layout,
+				),
+			)
 
 			// Pass the drawing operations to the GPU.
 			e.Frame(gtx.Ops)
