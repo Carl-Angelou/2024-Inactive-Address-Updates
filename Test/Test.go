a98239116a61ad06916ef2e269ae3c297e11c808
Carl <carlangelou06@gmail.com>
2024-02-12 13:16:15 -0500

    Added some comments

--- Test/Test.go
+++ Test/Test.go
@@ -11,7 +11,19 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-func main() {
+func LogOut(ctx context.Context) {
+	if err := chromedp.Run(ctx, 
+		// -------------------------- Log Out ------------------------------
+		chromedp.Click(".user_menu_icon"),
+		chromedp.Click(".menu_link.icon-logout"),
+	) 
+		
+	err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() { // STILL WORKS
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", false),
 		chromedp.Flag("disable-gpu", false),
@@ -47,11 +59,11 @@ func main() {
 	fmt.Printf("Columns: %d\n", len(columns))
 
 	// Get all Ref numbers
-	for _, row := range rows {
-		if len(row) > 0 { // Check if the row is not empty
-			fmt.Println(row[0]) // Print the value from the first column
-		}
-	}
+	// for _, row := range rows {
+	// 	if len(row) > 0 { // Check if the row is not empty
+	// 		fmt.Println(row[0]) // Print the value from the first column
+	// 	}
+	// }
 	
 	// Run chromedp
 	if err := chromedp.Run(ctx,
@@ -64,12 +76,22 @@ func main() {
 		chromedp.Sleep(1000*time.Millisecond),
 		chromedp.Click("#submitButton", chromedp.ByQuery), // Use cssSelector value (use SelectorsHub)
 		chromedp.Sleep(2000*time.Millisecond),
-		
 	)
-	
+
 	err != nil {
 		log.Fatal(err)
 	}
+
+	// for i := 0; i < len(rows); i++ {
+	// 	fmt.Print("")
+	// }
+
+	// Put some sort of UI here
+
+	LogOut(ctx)
+	
+	// prevent it from closing the browser immediately
+	time.Sleep(5000*time.Millisecond)
 
 	fmt.Println("Done")
 }
